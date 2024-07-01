fd3c3df44edf1231b77939dbb7bf8bd0fa8eb777
이상용 <lsy3709@naver.com>
2024-07-01 14:21:47 +0900

    5교시, 클래스의 매개변수에 보조생성자 호출해서, 주생성자 호출 연결 확인

--- app/src/main/java/com/busanit501/androidlabtest501/ch3_4_5/Test2.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/ch3_4_5/Test2.kt
@@ -6,21 +6,31 @@ class Test2 {
 //
 //class User(nameTest2:String, ageTest2:Int){
 // 매개변수 앞에 키워드 val 붙이면, 함수 내부에서도, 멤버 처럼 사용 가능.
-    class User(val nameTest2:String, val ageTest2:Int){
+//    class User(val nameTest2:String, val ageTest2:Int){
+class User(name: String){
     init {
         println("해당 클래스 인스턴스 주 생성자 이용해서 호출시, 반드시 실행됨.")
         // 주생성자의 매개변수는 , 기본은 init 영역 안에서 , 재사용가능.
-        println("name : ${nameTest2}, age : ${ageTest2}")
+//        println("init 안에서 사용, name : ${nameTest2}, age : ${ageTest2}")
+        println("주 생성자 호출1 User(name: String) : ")
+    }
+    constructor(name: String, age:Int): this(name) {
+        println("보조 생성자 호출2 constructor(name: String, age:Int) : ")
+    }
+    constructor(name: String, age: Int, email:String):this(name,age) {
+        println("보조 생성자 호출3 constructor(name: String, age:Int, email:String) : ")
     }
     //
     fun exFun() {
         // 다른 함수 내부에서는 사용이 불가.
-        println("name : ${nameTest2}, age : ${ageTest2}")
+//        println("exFun 안에서 사용 name : ${nameTest2}, age : ${ageTest2}")
     }
 }
 
 fun main() {
-    val lsy = User("이상용", 30)
-    lsy.exFun()
+    val lsy = User("이상용", 30,"lsy@naver.com")
+//    lsy.exFun()
+
+
 
 }
