e770fa18965df5402cb3eba03f9e213acf4ad19a
이상용 <lsy3709@naver.com>
2024-07-02 09:24:29 +0900

    1교시, 고차함수, 매개변수 함수모양, 반환 타입도 함수 모양, 테스트

--- app/src/main/java/com/busanit501/androidlabtest501/ch3_4_5/Test3.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/ch3_4_5/Test3.kt
@@ -2,11 +2,23 @@ package com.busanit501.androidlabtest501.ch3_4_5
 
 class Test3 {
 }
+// arg, 매개변수, 타입: 함수 타입, 모양은 정수 -> true,false
+// 함수의 반환 타입 : 또 함수 , 모양 : () -> String
+// fun sum (number:Int, number2:Int) : Int {}
+// 고차 함수, : 매개변수 또는 반환 타입에 함수 모양이 온다.
+fun hofFun(arg:(Int) -> Boolean):() -> String {
+    val result = if(arg(10)){
+        "valid"
+    } else {
+        "invalid"
+    }
+    return {"결과 : $result"}
+}
 
 fun main() {
     // 함수의 반환 타입이 없이 할당.
     val some = {no:Int -> println("no : $no") }
-    some(10)
+//    some(10)
 
     //it
     // 함수타입 존재
@@ -14,6 +26,10 @@ fun main() {
     // 구조를 잘 봐두어야 함.
     // 네트워크 프로그래밍 구현시,
     val some2:(Int) -> Unit = {println("no : $it") }
-    some2(100)
+//    some2(100)
+
+    //고차 함수 사용하기, 인자 : 함수를 넣기
+    val result = hofFun ({no -> no > 0  })
+    println("고차함수 결괏값 조회 : ${result}")
 
 }
