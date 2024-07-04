8b741dde965f18c7e07acd8576021686e01b5ab7
이상용 <lsy3709@naver.com>
2024-07-04 10:28:46 +0900

    2교시, frameLayout 기본 2, 뼈대

--- app/src/main/java/com/busanit501/androidlabtest501/ch07/Ch07MainFrameActivity.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/ch07/Ch07MainFrameActivity.kt
@@ -6,16 +6,26 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.busanit501.androidlabtest501.R
+import com.busanit501.androidlabtest501.databinding.ActivityCh07MainFrameBinding
 
 class Ch07MainFrameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_ch07_main_frame)
+
+        //바인딩, 뷰들을 , 하나의 인스턴스에 담아서, 꺼내서 사용하기.
+        val binding = ActivityCh07MainFrameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        setContentView(R.layout.activity_ch07_main_frame)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
+
+
+
+
+    }// onCreate
 }
