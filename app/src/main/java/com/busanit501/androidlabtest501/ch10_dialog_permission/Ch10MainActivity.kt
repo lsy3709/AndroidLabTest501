2f850bfd46d4ad8941c1eb52980c1d5008ce1d26
이상용 <lsy3709@naver.com>
2024-07-05 10:28:49 +0900

    2교시, ch10 퍼미션 확인3, 셀프 퍼미션 체크 해보기, 로직 처리

--- app/src/main/java/com/busanit501/androidlabtest501/ch10_dialog_permission/Ch10MainActivity.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/ch10_dialog_permission/Ch10MainActivity.kt
@@ -1,21 +1,44 @@
 package com.busanit501.androidlabtest501.ch10_dialog_permission
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.busanit501.androidlabtest501.R
+import com.busanit501.androidlabtest501.databinding.ActivityCh10MainBinding
 
 class Ch10MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityCh10MainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_ch10_main)
+
+        binding = ActivityCh10MainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        setContentView(R.layout.activity_ch10_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
+        //퍼미션 체크 해보기.
+        val status = ContextCompat.checkSelfPermission(this@Ch10MainActivity,
+            "android.permission.ACCESS_FINE_LOCATION")
+
+        if(status == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this@Ch10MainActivity,"위치 권한 승인됨", Toast.LENGTH_LONG).show()
+
+        } else {
+            Toast.makeText(this@Ch10MainActivity,"위치 권한 승인 안됨", Toast.LENGTH_LONG).show()
+        }
+
+
+
+    }// onCreate
 }
