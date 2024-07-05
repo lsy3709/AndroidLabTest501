435af3118245baaccdacebaa76aec3fd893e1202
이상용 <lsy3709@naver.com>
2024-07-05 11:17:02 +0900

    3교시, ch10 다이얼로그 창에서, 토스트 콜백함수 로직 처리, 나타날 경우 및 사라질 경우 후처리

--- app/src/main/java/com/busanit501/androidlabtest501/ch10_dialog_permission/Ch10MainActivity.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/ch10_dialog_permission/Ch10MainActivity.kt
@@ -1,26 +1,34 @@
 package com.busanit501.androidlabtest501.ch10_dialog_permission
 
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.busanit501.androidlabtest501.R
 import com.busanit501.androidlabtest501.databinding.ActivityCh10MainBinding
+import com.busanit501.androidlabtest501.miniProject.test0703.lsy1205_mini.Lsy1205MainActivity
 
 class Ch10MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityCh10MainBinding
+    lateinit var TAG:String
 
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         binding = ActivityCh10MainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        TAG = "Ch10MainActivity"
 
 //        setContentView(R.layout.activity_ch10_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -56,5 +64,28 @@ class Ch10MainActivity : AppCompatActivity() {
         requestPermissionLauncher.launch("android.permission.ACCESS_FINE_LOCATION")
 
 
+        // 토스트 메시지가 뜨고나서 후처리 액션, 사라지고 나서 후처리 액션,
+        val toast = Toast.makeText(this@Ch10MainActivity,"토스트 메세지 출력 또는 사라질 경우 후처리 동작 확인", Toast.LENGTH_LONG)
+
+        //콜백 함수 추가하기.
+        toast.addCallback(
+            @RequiresApi(Build.VERSION_CODES.R)
+            object : Toast.Callback() {
+                //나타 날 때, 후처리 동작 추가
+                override fun onToastShown() {
+                    super.onToastShown()
+                    Log.d(TAG,"토스가 나타날 때 동작하는 로그")
+                    //화면 이동도 추가해보기.
+                    val intent = Intent(this@Ch10MainActivity, Lsy1205MainActivity::class.java)
+                    startActivity(intent)
+                }
+                //사라질 때, 후처리 동작 추가
+                override fun onToastHidden() {
+                    super.onToastHidden()
+                    Log.d(TAG,"토스가 사라질 때 동작하는 로그")
+                }
+            }
+        )
+toast.show()
     }// onCreate
 }
