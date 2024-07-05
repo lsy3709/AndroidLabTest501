115c4b0cbfd5f92428aa8765e0eedbb015851aac
이상용 <lsy3709@naver.com>
2024-07-05 16:40:44 +0900

    7교시, ch10 알림 띄우기 테스트3, 옵션 확인2, 알림 창에서 추가 액션 넣기3

--- app/src/main/java/com/busanit501/androidlabtest501/ch10_dialog_permission/ReplyReceiver.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/ch10_dialog_permission/ReplyReceiver.kt
@@ -1,13 +1,44 @@
 package com.busanit501.androidlabtest501.ch10_dialog_permission
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.RemoteInput
 
 class ReplyReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        TODO("ReplyReceiver.onReceive() is not implemented")
+        val replyText = RemoteInput.getResultsFromIntent(intent)?.getCharSequence("key_text_reply")
+
+        val manager = context.getSystemService(AppCompatActivity.NOTIFICATION_SERVICE) as NotificationManager
+        val builder : NotificationCompat.Builder
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channelId = "one-channel"
+            val channelName = "My Channel One"
+            val channel = NotificationChannel(
+                channelId,channelName,NotificationManager.IMPORTANCE_HIGH
+            )
+            manager.createNotificationChannel(channel)
+
+            builder = NotificationCompat.Builder(context, channelId)
+        } else {
+            builder = NotificationCompat.Builder(context)
+
+            builder.setSmallIcon(android.R.drawable.ic_notification_overlay)
+            builder.setWhen(System.currentTimeMillis())
+            builder.setContentTitle("Content Title")
+            builder.setContentText("Content Massage")
+
+            manager.notify(11, builder.build())
+        }
+
+
     }
 }
