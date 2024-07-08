47243e45784d83dbeda9e5d09a1e5d05eafc0fe7
이상용 <lsy3709@naver.com>
2024-07-08 11:36:55 +0900

    3교시 11 제트팩, 액션바, 뒤로가기 버튼 업버튼 확인

--- app/src/main/java/com/busanit501/androidlabtest501/ch11_jetpack/Ch11Main2Activity.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/ch11_jetpack/Ch11Main2Activity.kt
@@ -6,26 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.busanit501.androidlabtest501.R
-import com.busanit501.androidlabtest501.databinding.ActivityCh11MainBinding
 
-class Ch11MainActivity : AppCompatActivity() {
-
-    lateinit var binding : ActivityCh11MainBinding
-
+class Ch11Main2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityCh11MainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-//        setContentView(R.layout.activity_ch11_main)
+        setContentView(R.layout.activity_ch11_main2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        //업버튼 화면 붙이기, 이벤트 작업 해보기.
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-    } //onCreate
+    }
 }
