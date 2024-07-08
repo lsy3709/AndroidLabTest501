7177bdaef3e6ff54c9be77a2167e9455e231e980
이상용 <lsy3709@naver.com>
2024-07-08 15:00:50 +0900

    6교시 11 제트팩, 프래그먼트, 액티비티에 프래그먼트 붙이기 작업 준비2

--- app/src/main/java/com/busanit501/androidlabtest501/ch11_jetpack/Ch11MainFragmentActivity.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/ch11_jetpack/Ch11MainFragmentActivity.kt
@@ -6,19 +6,28 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.busanit501.androidlabtest501.R
+import com.busanit501.androidlabtest501.databinding.ActivityCh11MainFragmentBinding
 
 class Ch11MainFragmentActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityCh11MainFragmentBinding
 
     //순서1, 프래그먼트 그리기 위한 빈 도화지 역할. 액티비티 만들기
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_ch11_main_fragment)
+        binding = ActivityCh11MainFragmentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        setContentView(R.layout.activity_ch11_main_fragment)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-}
+
+
+
+    } //onCreate
+} //
--- app/src/main/java/com/busanit501/androidlabtest501/ch11_jetpack/fragments/Ch11ThreeFragment.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/ch11_jetpack/fragments/Ch11ThreeFragment.kt
@@ -7,20 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import com.busanit501.androidlabtest501.R
 import com.busanit501.androidlabtest501.databinding.FragmentCh11OneBinding
+import com.busanit501.androidlabtest501.databinding.FragmentCh11ThreeBinding
 import com.busanit501.androidlabtest501.databinding.FragmentCh11TwoBinding
 
-class Ch11TwoFragment : Fragment() {
+class Ch11ThreeFragment : Fragment() {
 
 
     // 순서2, 프래그먼트 만들기. 뼈대 샘플 코딩 많이 지움.
 
-    lateinit var binding: FragmentCh11TwoBinding
+    lateinit var binding: FragmentCh11ThreeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentCh11TwoBinding.inflate(layoutInflater,container,false)
+        binding = FragmentCh11ThreeBinding.inflate(layoutInflater,container,false)
 
 //        return inflater.inflate(R.layout.fragment_ch11_one, container, false)
         return binding.root
