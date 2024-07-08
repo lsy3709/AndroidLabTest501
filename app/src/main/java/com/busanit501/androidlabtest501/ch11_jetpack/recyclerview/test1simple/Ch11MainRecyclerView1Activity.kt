186793dd8257235771693afc3ca77b156f1bf5c6
이상용 <lsy3709@naver.com>
2024-07-08 16:45:30 +0900

    7교시 11 제트팩, 프래그먼트, 액티비티에 프래그먼트 붙이기 작업 준비6, 프래그먼트 생명주기 보여주기.4 확인

---
+++ app/src/main/java/com/busanit501/androidlabtest501/ch11_jetpack/recyclerview/test1simple/Ch11MainRecyclerView1Activity.kt
@@ -0,0 +1,22 @@
+package com.busanit501.androidlabtest501.ch11_jetpack.recyclerview.test1simple
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.busanit501.androidlabtest501.R
+
+// 순서1, 리사이클러뷰 출력할 빈 도화지 역할.
+class Ch11MainRecyclerView1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ch11_main_recycler_view1)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/busanit501/androidlabtest501/ch11_jetpack/recyclerview/test1simple/Ch11MyAdapterSample.kt
@@ -0,0 +1,19 @@
+package com.busanit501.androidlabtest501.ch11_jetpack.recyclerview.test1simple
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.busanit501.androidlabtest501.databinding.Ch11ItemSimpleRecyclerBinding
+
+class Ch11MyAdapterSample(val binding:Ch11ItemSimpleRecyclerBinding ) :RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
