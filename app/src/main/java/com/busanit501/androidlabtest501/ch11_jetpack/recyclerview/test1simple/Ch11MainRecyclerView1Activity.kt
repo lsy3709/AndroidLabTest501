b8514b6d3c2252eb25534e56d76eeda002c19c52
이상용 <lsy3709@naver.com>
2024-07-09 09:39:54 +0900

    1교시 11 제트팩, 리사이클러뷰 ,액티비티에서 뷰 배치모양, 어댑터에 데이터 담아서 연결하기

--- app/src/main/java/com/busanit501/androidlabtest501/ch11_jetpack/recyclerview/test1simple/Ch11MainRecyclerView1Activity.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/ch11_jetpack/recyclerview/test1simple/Ch11MainRecyclerView1Activity.kt
@@ -5,7 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.busanit501.androidlabtest501.R
+import com.busanit501.androidlabtest501.ch3_4_5.data
 import com.busanit501.androidlabtest501.databinding.ActivityCh11MainRecyclerView1Binding
 
 // 순서1, 리사이클러뷰 출력할 빈 도화지 역할.
@@ -24,5 +26,23 @@ class Ch11MainRecyclerView1Activity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
+
+        //샘플 더미 데이터 넣기.
+        val datas = mutableListOf<String>()
+        for(i in 1..10) {
+            datas.add("오늘 점심 뭐먹지 ? $i")
+        }
+
+        //리사이클러뷰 , 어떤 모양으로 배치할지 정하기.
+        // 세로 방향, 가로 방향, 지그재그, 그리드 등.
+        val layoutManager = LinearLayoutManager(this@Ch11MainRecyclerView1Activity)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        binding.ch11RecyclerSample1.layoutManager = layoutManager
+
+        // 어댑터 붙이기.
+        binding.ch11RecyclerSample1.adapter = Ch11MyAdapterSample(datas)
+
+
+
+    } //onCreate
 }
