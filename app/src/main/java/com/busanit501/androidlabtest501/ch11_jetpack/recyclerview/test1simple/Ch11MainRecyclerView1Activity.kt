d8258c857082978a71ca8c423d270ec33a95cc96
이상용 <lsy3709@naver.com>
2024-07-09 09:21:47 +0900

    1교시 11 제트팩, 리사이클러뷰 , 액티비티 화면에 리사이클러뷰 붙이기, 목록 요소 뷰 정하기

--- app/src/main/java/com/busanit501/androidlabtest501/ch11_jetpack/recyclerview/test1simple/Ch11MainRecyclerView1Activity.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/ch11_jetpack/recyclerview/test1simple/Ch11MainRecyclerView1Activity.kt
@@ -6,13 +6,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.busanit501.androidlabtest501.R
+import com.busanit501.androidlabtest501.databinding.ActivityCh11MainRecyclerView1Binding
 
 // 순서1, 리사이클러뷰 출력할 빈 도화지 역할.
 class Ch11MainRecyclerView1Activity : AppCompatActivity() {
+    lateinit var binding: ActivityCh11MainRecyclerView1Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_ch11_main_recycler_view1)
+        binding = ActivityCh11MainRecyclerView1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        setContentView(R.layout.activity_ch11_main_recycler_view1)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
