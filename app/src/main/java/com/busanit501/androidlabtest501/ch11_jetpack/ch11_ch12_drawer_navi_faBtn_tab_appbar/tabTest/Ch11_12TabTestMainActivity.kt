b9d10b8588076be837d83fcdd2cbdfce52bfce8f
이상용 <lsy3709@naver.com>
2024-07-11 11:46:06 +0900

    3교시, 11_12, 탭레이아웃, 프래그먼트 구성 1 화면 작업 ,액티비티, 뷰페이저 확인

--- app/src/main/java/com/busanit501/androidlabtest501/ch11_jetpack/ch11_ch12_drawer_navi_faBtn_tab_appbar/tabTest/Ch11_12TabTestMainActivity.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/ch11_jetpack/ch11_ch12_drawer_navi_faBtn_tab_appbar/tabTest/Ch11_12TabTestMainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.busanit501.androidlabtest501.R
+import com.busanit501.androidlabtest501.ch11_jetpack.ch11_ch12_drawer_navi_faBtn_tab_appbar.ViewPageAdapterTest
 import com.busanit501.androidlabtest501.databinding.ActivityCh1112TabTestMainBinding
 
 class Ch11_12TabTestMainActivity : AppCompatActivity() {
@@ -23,6 +24,16 @@ class Ch11_12TabTestMainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // 뷰페이저 어댑터 연결 필요. 어댑터 어제 만들었던 뷰페이저용, 어댑터 재사용.
+        binding.ch1112TabViewpager1.adapter = ViewPageAdapterTest(this@Ch11_12TabTestMainActivity)
+
+
+        // 탭 레이아웃 이벤트 및 탭 연결 작업 필요.
+
+        // 탭에 우리가 지정한 아이콘 붙이기 작업.
+
+
 
 
     } //onCreate
