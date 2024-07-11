19786d94a1b109380b5f1fd04f770e1f0a3b5674
이상용 <lsy3709@naver.com>
2024-07-11 14:23:43 +0900

    5교시, 13 액티비티, 인텐트, 데이터 전달 예제

---
+++ app/src/main/java/com/busanit501/androidlabtest501/ch13_intent/TestBundleActivity.kt
@@ -0,0 +1,85 @@
+package com.busanit501.androidlabtest501.ch13_intent
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.busanit501.androidlabtest501.R
+import com.busanit501.androidlabtest501.databinding.ActivityTestBundleBinding
+
+class TestBundleActivity : AppCompatActivity() {
+    lateinit var binding : ActivityTestBundleBinding
+    var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTestBundleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.d("lsy","onCreate()")
+
+        // 화면이 회전시 데이터 손실이 되는데,
+        // 임시 저장소 번들에 저장하고, 값을 가져오기.
+        // 앱을 사용중에는 기본 값들을 세터 및 게터가 용이하고,
+        // 앱이 종료하면, 휘발성 단점.
+        // 비휘발성, 실제 내부 저장소를 사용할 예정
+        // 1) 파일 2) 데이터베이스 (SQLite) 3) 외부 디비를 사용할 예정.
+
+        binding.countBtn.setOnClickListener {
+            count++
+            binding.resultText.text = "$count"
+        }
+
+        // onCreate 마지막 블럭 부분
+    }
+    // onCreate 밖에서,
+    // 번들 객체 이용함.
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d("lsy","onSaveInstanceState()")
+        // 데이터 세터 , onDestroy()전에 저장하고.
+        outState.putString("data1","Hello~~~~~")
+        outState.putInt("data2", 20)
+        outState.putInt("data3",count)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d("lsy","onRestoreInstanceState()")
+        // 데이터 게터 onResume() 전에 불러오고
+        val data1 = savedInstanceState.getString("data1")
+        val data2 = savedInstanceState.getInt("data2")
+        val data3 = savedInstanceState.getInt("data3")
+        binding.resultText.text = "$data1 - $data2"
+        binding.resultText2.text ="count 임시저장 값 : $data3"
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("lsy","onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("lsy","onResume()")
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("lsy","onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("lsy","onStop()")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("lsy","onDestroy()")
+    }
+
+}
---
+++ app/src/main/java/com/busanit501/androidlabtest501/ch13_intent/TestCoroutineActivity.kt
@@ -0,0 +1,97 @@
+package com.busanit501.androidlabtest501.ch13_intent
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.busanit501.androidlabtest501.R
+import com.busanit501.androidlabtest501.databinding.ActivityTestCoroutineBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.launch
+import kotlin.system.measureTimeMillis
+
+class TestCoroutineActivity : AppCompatActivity() {
+    lateinit var binding : ActivityTestCoroutineBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTestCoroutineBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.testCoBtn.setOnClickListener {
+
+            //오래걸리는 작업 추가. -> 화면에서 처리하기보다는 -> 비동기식으로 처리를 하겠다.
+            // 비동기식으로 처리하는 방법이 기존에 스레드(핸들러작업), 코루틴으로 작업하는 부분 소개.
+
+            // binding.resultCoView.text = "time: $time, sum : $sum"
+
+
+            //방법1, 비동기 작업, Handler 방식, deprecated , 참고만,
+//            val handler = object : Handler() {
+//                override fun handleMessage(msg: Message) {
+//                    super.handleMessage(msg)
+//                    binding.resultCoView.text = "time: ${msg.arg2}, sum : ${msg.arg1}"
+//                }
+//            }
+
+            // thread 작업, 비동기 작업. 코루틴에 비해서 성능이 조금 무겁다.
+//            thread {
+//                // 무거운 작업(오래 걸리는 작업 넣기.)
+//                var sum = 0L
+//                var time = measureTimeMillis {
+//                    for(i in 1..9_000_000_000){
+//                        sum += i
+//                    }
+//                }
+//                //  오래 걸리는 작업의 결과값을 메세지를 통해서, 핸들러에게 전달하는 로직.
+//                val message = Message()
+//                message.arg1 = sum.toInt()
+//                message.arg2 = time .toInt()
+//                handler.sendMessage(message)
+//
+//                Log.d("lsy","time : $time")
+//            }
+
+
+            // 방법2, 코루틴 이용해서, 비동기 처리 해보기.
+            // 무거운 작업의 결괏값을 전달하는 도구.
+            val channel = Channel<Int> ()
+            // 무건운 작업을 수행하는 로직.
+            // Dispatchers.Default : 백그라운드 CPU 연산을 많이 할 때 사용하는 케이스
+            val backgroundScop = CoroutineScope(Dispatchers.Default + Job())
+            backgroundScop.launch {
+                // 무거운 작업 추가.
+                var sum = 0L
+                var time = measureTimeMillis {
+                    for(i in 1..9_000_000_000){
+                        sum += i
+                    }
+                }
+                Log.d("lsy","time : $time")
+                channel.send(sum.toInt())
+//                channel.send(time.toInt())
+            }
+
+            // 화면 출력을 담당하는 MAIN 을 구축.
+            // 무거운 작업에서 결괏값을 받아서 출력을 함.
+            val mainScope = GlobalScope.launch(Dispatchers.Main) {
+                channel.consumeEach {
+                    binding.resultCoView.text = "sum : $it"
+                }
+            }
+
+        }
+        // testCoBtn setOnClick
+
+
+
+
+    }
+    // onCreate
+}
