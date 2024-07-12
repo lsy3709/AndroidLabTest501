8154a59899586d9e72bbc1f230d869eada466647
이상용 <lsy3709@naver.com>
2024-07-12 10:16:19 +0900

    2교시 ch16 콘텐츠 프로바이더 외부앱 연동, 연락처 연동해보기3

--- app/src/main/java/com/busanit501/androidlabtest501/ch16_contentProvider/Ch16ContactMainActivity.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/ch16_contentProvider/Ch16ContactMainActivity.kt
@@ -30,16 +30,38 @@ class Ch16ContactMainActivity : AppCompatActivity() {
 
         // 후처리 후, 자동 호출되는 함수
         val requestContactsLauncher = registerForActivityResult(
+            // 후처리, 2번 앱에서 가져온 데이터를 처리하는 로직.
             ActivityResultContracts.StartActivityForResult()
         ) {
             if (it.resultCode == RESULT_OK) {
                 Log.d("lsy", "${it.data?.data}")
+                //실제 데이터를 불러와서, 결과뷰에 넣어보기.
+                val cursor = contentResolver.query(
+                    it!!.data!!.data!!,
+                    arrayOf<String>(
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                        ContactsContract.CommonDataKinds.Phone.NUMBER
+                    ),
+                    null,
+                    null,
+                    null,
+                ) //cursor
+                Log.d("lsy", "cursor size: ${cursor?.count}")
+                // cursor 에는 , 마치 디비에서 select 를 하는 방법과 비슷
+                if(cursor!!.moveToFirst()) {
+                    val name = cursor?.getString(0)
+                    val phone = cursor?.getString(1)
+                    binding.ch16ResultView.text = "이름 : $name, 전화번호: $phone"
+                }
+
             }
         }
 
         //이벤트 처리
         binding.ch16ContactBtn.setOnClickListener {
+            // 연락처 앱으로 접근
             val intent = Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
+            // 인텐트를 전달, 시스템에게, 후처리
             requestContactsLauncher.launch(intent)
         }
 
