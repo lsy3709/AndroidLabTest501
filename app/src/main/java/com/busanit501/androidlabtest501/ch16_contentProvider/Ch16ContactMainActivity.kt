41133ec3f3e463164d62c3d11f7cc5aa3ebbd529
이상용 <lsy3709@naver.com>
2024-07-12 09:57:57 +0900

    1교시 ch16 콘텐츠 프로바이더 외부앱 연동, 연락처 연동해보기2

--- app/src/main/java/com/busanit501/androidlabtest501/ch16_contentProvider/Ch16ContactMainActivity.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/ch16_contentProvider/Ch16ContactMainActivity.kt
@@ -1,7 +1,11 @@
 package com.busanit501.androidlabtest501.ch16_contentProvider
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -24,7 +28,23 @@ class Ch16ContactMainActivity : AppCompatActivity() {
             insets
         }
 
+        // 후처리 후, 자동 호출되는 함수
+        val requestContactsLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            if (it.resultCode == RESULT_OK) {
+                Log.d("lsy", "${it.data?.data}")
+            }
+        }
+
+        //이벤트 처리
+        binding.ch16ContactBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
+            requestContactsLauncher.launch(intent)
+        }
+
 
 
     }//onCreate
+
 }
