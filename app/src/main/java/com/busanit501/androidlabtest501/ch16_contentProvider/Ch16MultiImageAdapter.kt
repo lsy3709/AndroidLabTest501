d4c520b22699bc478589d5e76899231b1fcfa05c
이상용 <lsy3709@naver.com>
2024-07-12 09:24:19 +0900

    0712, 1교시 ch16 콘텐츠 프로바이더 준비1

---
+++ app/src/main/java/com/busanit501/androidlabtest501/ch16_contentProvider/Ch16MultiImageAdapter.kt
@@ -0,0 +1,57 @@
+package com.busanit501.androidlabtest501.ch16_contentProvider
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.busanit501.androidlabtest501.databinding.Ch16MutilimagepickItemBinding
+
+class MyViewHolder (val binding: Ch16MutilimagepickItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+// 추가, 어댑터 매개변수에 context 를 추가하면,
+// 해당 액티비티나, 프래그먼트에서 뷰 작업하기 좋음.
+// 현재 사용이된 곳, glide 사진 출력 라이브러리 , with(this) -> context 사용이됨.
+// 토스트 출력시 this -> context , 사용이됨.
+class Ch16MultiImageAdapter (val datas: ArrayList<Uri>, val context: Context)
+    : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        MyViewHolder(
+            Ch16MutilimagepickItemBinding.inflate(
+                LayoutInflater.from(parent.context),parent,false))
+
+    override fun getItemCount(): Int {
+        return datas.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as MyViewHolder).binding
+        // glide 를 이용해서, 이미지를 처리하기.
+        // glide : 코루틴이 적용이 되어서, 비동기식으로 처리해서 속도도 빠르고,
+        // 이미지 출력도 쉽고, 크기 조절도 쉬워요.
+        // 외부 기능을 가져다 사용 할게요.
+        // 데이터에서 각요소 가져오기.
+        Glide.with(context).load(datas[position])
+            //크기 조절
+            .override(200,200)
+            // 결과 이미지 넣기
+            .into(binding.multiImageItem)
+
+        // 이벤트 처리할 때 필요한 부분
+        binding.itemRoot.setOnClickListener {
+            Log.d("lsy", "item clicked : $position")
+            if (position == 0) {
+                Log.d("lsy", "0번 요소 item clicked : $position")
+                Toast.makeText(context,"0번 요소 item clicked : $position",Toast.LENGTH_SHORT).show()
+                // 상세 페이지 이동으로 사용해도 됨.
+            } else {
+                Log.d("lsy", "0번 요소 외 item clicked : $position")
+                Toast.makeText(context,"0번 요소 외 item clicked : $position",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/busanit501/androidlabtest501/ch16_contentProvider/Ch16MultiImageRecyclerActivity.kt
@@ -0,0 +1,104 @@
+package com.busanit501.androidlabtest501.ch16_contentProvider
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.busanit501.androidlabtest501.R
+import com.busanit501.androidlabtest501.databinding.ActivityCh16MultiImageRecyclerBinding
+
+class Ch16MultiImageRecyclerActivity : AppCompatActivity() {
+    // 전역
+    lateinit var binding : ActivityCh16MultiImageRecyclerBinding
+    // 사진의 위치 Uri를 가지는 리스트를 하나 만들기.
+    var list = ArrayList<Uri>()
+    var adapter = MultiImageAdapter(list,this)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCh16MultiImageRecyclerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 갤러리 버튼 클릭시, 이벤트 처리하는 부분, -> 후처리, 재사용하고, 속성 몇개 고치기
+        // 두번째, 갤러리 앱에 데이터를 가져온 내용을 처리하는 작업.
+
+        val requestGalleryLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+
+            // 추가
+            list.clear()
+            // 사진 여러 장을 선택시 사용할 키워드 : clipData
+            if(it.data?.clipData != null) {
+                // 선택된 사진 갯수
+                // 10장까지 제한하는 로직
+                val count = it.data!!.clipData?.itemCount
+                if ( count !=null) {
+                    if(count > 10) {
+                        Toast.makeText(this,"사진은 10장까지만 가능",Toast.LENGTH_SHORT).show()
+                    }
+                }
+                // 10장까지 제한하는 로직
+
+                // 반복문으로 , 사진 데이터의 Uri 조회해서 -> 리스트에 담기
+                for(i in 0 until count!!) {
+                    // 여러 사진이 이터러블 (반복가능한 형태, 인덱스 있다.) 각 요소의 사진 Uri 가져오기.
+                    val imageUri = it.data!!.clipData?.getItemAt(i)?.uri
+                    Log.d("lsy","사진의 위치 Uri : $imageUri")
+                    if (imageUri != null) {
+                        list.add(imageUri)
+                    }
+                }
+                // 반복문으로 , 사진 데이터의 Uri 조회해서 -> 리스트에 담기
+
+                //  if(it.data?.clipData != null) { 의 반대 경우, 한장 인경우
+            } else {
+                it.data.let {
+                        uri -> val imageUri : Uri? = it.data?.data
+                    if (imageUri != null) {
+                        Log.d("lsy","사진의 위치 Uri : $imageUri")
+                        list.add(imageUri)
+                    }
+                }
+            }
+            // 데이터 변경 유무를 알리는 메서드 , 전체가 다 성능 좋지 않음. 테스트용.
+            adapter.notifyDataSetChanged()
+
+        }
+        // 후처리 닫는 블록
+
+        // 버튼클릭시 : 갤러리 앱 호출,
+        binding.multiGalleryBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            // 사진을 여러장 고르는 속성 추가. 시스템에게 사진 여러장 선택 한다는 신고.
+            // 추가1
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true)
+            // image 이고, images 오타가 있으면 안됨. 사진 선택이 안됨.
+            intent.setType("image/*")
+
+            // 사진에 접근 하겠다는 정해진 액션 문자열
+            // 추가2
+            intent.action = Intent.ACTION_GET_CONTENT
+            requestGalleryLauncher.launch(intent)
+
+        }
+
+        // 리사이클러뷰 준비 재료 1) 어댑터 2) 뷰홀더 3) 목록 요소의 레이아웃 구성.
+        val layoutManager = LinearLayoutManager(this)
+        binding.multiImagePickRecycler.layoutManager = layoutManager
+        binding.multiImagePickRecycler.adapter = adapter
+
+
+    }
+    // onCreate
+
+
+}
